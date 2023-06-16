2ee2475f59fbf8deb996d4e446b483be9a552062
Christian Ek <christian.ek@norsefeedback.no>
2023-06-16 08:48:09 +0200

    Refactor device to use try catch instead of .then

--- drivers/heat-pump/device.js
+++ drivers/heat-pump/device.js
@@ -7,14 +7,11 @@ const ErrorCodes = require('../../lib/errorcodes');
 
 class HeatPumpDevice extends Device {
 
-  /**
-   * onInit is called when the device is initialized.
-   */
   async onInit() {
     try {
       this.client = await this.getClient(this.getSettings());
     } catch (e) {
-      this.log(`unable to initialize device: ${e.message}`);
+      this.log(`Unable to initialize device: ${e.message}`);
       throw e;
     }
 
@@ -31,26 +28,21 @@ class HeatPumpDevice extends Device {
   }
 
   async getDeviceData() {
-    this.log(`[${this.getName()}][${this.data.id}]`, 'Refresh device');
+    this.log(`[${this.getName()}][${this.data.id}]`, 'Refreshing device data');
 
     for (const value of Object.values(Capabilities)) {
-      await this.client.get(value.endpoint)
-        .then((res) => {
-          this.updateValue(value.name, res.value);
-        })
-        .catch((err) => this.log(err));
+      try {
+        const res = await this.client.get(value.endpoint);
+        this.updateValue(value.name, res.value);
+      } catch (err) {
+        this.log(err);
+      }
     }
   }
 
   updateValue(capability, value) {
     if (capability === 'alarm_status') {
-      if (value === 'ok') {
-        value = false;
-      } else {
-        // Activate alarm
-        value = true;
-      }
-
+      value = value !== 'ok';
       if (this.getCapabilityValue(capability) !== value) {
         this.triggerAlarmStatusChange(value);
       }
@@ -62,26 +54,24 @@ class HeatPumpDevice extends Device {
 
   async triggerAlarmStatusChange(value) {
     if (value) {
-      await this.client.get('/notifications')
-        .then((res) => {
-          const tokens = {
-            code: res.values.map((obj) => obj.ccd).join(', '),
-            description: res.values.map((obj) => {
-              return `${obj.ccd}: ${ErrorCodes[obj.ccd].description}`;
-            }).join(', '),
-          };
-          return tokens;
-        })
-        .then((tokens) => {
-          this.log('Alarm status has changed to error. Trigger ERROR card..');
-          this.log(`code: ${tokens.code}`);
-          this.log(`description: ${tokens.description}`);
+      try {
+        const res = await this.client.get('/notifications');
+        const tokens = {
+          code: res.values.map((obj) => obj.ccd).join(', '),
+          description: res.values
+            .map((obj) => `${obj.ccd}: ${ErrorCodes[obj.ccd].description}`)
+            .join(', '),
+        };
 
-          this.homey.flow.getDeviceTriggerCard('alarm_status_error')
-            .trigger(this, tokens)
-            .catch(this.error);
-        })
-        .catch(this.error);
+        this.log('Alarm status has changed to error. Trigger ERROR card..');
+        this.log(`code: ${tokens.code}`);
+        this.log(`description: ${tokens.description}`);
+
+        await this.homey.flow.getDeviceTriggerCard('alarm_status_error')
+          .trigger(this, tokens);
+      } catch (error) {
+        this.log(error);
+      }
     } else if (this.getCapabilityValue('alarm_status') === 'error') {
       this.log('Alarm status has changed to OK. Trigger OK card.');
       this.homey.flow.getDeviceTriggerCard('alarm_status_ok').trigger(this)
@@ -89,68 +79,44 @@ class HeatPumpDevice extends Device {
     }
   }
 
-  /**
-   * onAdded is called when the user adds the device, called just after pairing.
-   */
   async onAdded() {
-    this.log('device added');
-    this.log('name:', this.getName());
-    this.log('class:', this.getClass());
-    this.log('data', this.getData());
+    this.log('Device added');
+    this.log('Name:', this.getName());
+    this.log('Class:', this.getClass());
+    this.log('Data:', this.getData());
   }
 
-  /**
-   * onSettings is called when the user updates the device's settings.
-   * @param {object} event the onSettings event data
-   * @param {object} event.oldSettings The old settings object
-   * @param {object} event.newSettings The new settings object
-   * @param {string[]} event.changedKeys An array of keys changed since the previous version
-   * @returns {Promise<string|void>} return a custom message that will be displayed
-   */
-  async onSettings({
-    oldSettings,
-    newSettings,
-    changedKeys,
-  }) {
+  async onSettings({ oldSettings, newSettings, changedKeys }) {
     const { interval } = this;
     for (const name of changedKeys) {
-      /* Log setting changes except for password */
       if (name !== 'password') {
-        this.log(`Setting '${name}' set '${oldSettings[name]}' => '${newSettings[name]}'`);
+        this.log(`Setting '${name}' changed from '${oldSettings[name]}' to '${newSettings[name]}'`);
       }
     }
     if (oldSettings.interval !== newSettings.interval) {
-      this.log(`Delete old interval of ${oldSettings.interval}s and creating new ${newSettings.interval}s`);
+      this.log(`Deleting old interval of ${oldSettings.interval}s and creating new ${newSettings.interval}s`);
       clearInterval(interval);
       this.setUpdateInterval(newSettings.interval);
     }
   }
 
-  // Get a (connected) instance of the Nefit Easy client.
   async getClient(settings) {
     const client = IVTClient({
       serialNumber: settings.serial,
       accessKey: settings.key,
       password: settings.password,
     });
-    await client.connect()
-      .then(this.log('device connected successfully to backend'));
+
+    await client.connect();
+    this.log('Device connected successfully to backend');
 
     return client;
   }
 
-  /**
-   * onRenamed is called when the user updates the device's name.
-   * This method can be used this to synchronise the name to the device.
-   * @param {string} name The new name
-   */
   async onRenamed(name) {
     this.log(`${name} renamed`);
   }
 
-  /**
-   * onDeleted is called when the user deleted the device.
-   */
   async onDeleted() {
     const { interval } = this;
     this.log(`${this.data.id} deleted`);
